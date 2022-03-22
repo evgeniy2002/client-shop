630fbf016c803461bef9e9c5261e62eda46ab407
Evgenij Kobzev <you@gmail.com>
2022-03-22 18:56:17 +0300

    new client v10.0.0(4)

--- src/http/index.js
+++ src/http/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const host = axios.create({
   withCredentials: true,
-  baseURL: 'https://shrouded-reaches-17656.herokuapp.com/',
+  baseURL: 'https://shrouded-reaches-17656.herokuapp.com',
   headers: {'Content-Type': 'application/json'}
   // baseURL: 'http://localhost:5000/'
 })
