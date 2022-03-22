a02bc5550d8c669139e49a00ec18997b4ce9b583
Evgenij Kobzev <you@gmail.com>
2022-03-22 21:21:17 +0300

    new client v10.0.0(15)

--- src/http/index.js
+++ src/http/index.js
@@ -4,12 +4,12 @@ const host = axios.create({
   // withCredentials: true,
   baseURL: 'https://shrouded-reaches-17656.herokuapp.com/',
   withCredentials: true,
-  // headers: {
-  //   'Access-Control-Allow-Origin' : '*',
-  //   'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
-  //   'Content-Type': 'application/json',
-  //   'Access-Control-Allow-Credentials': true
-  //   }
+  headers: {
+    'Access-Control-Allow-Origin' : '*',
+    'Access-Control-Allow-Methods':'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Credentials': true
+    }
   // headers: {
   //   'Content-Type': 'application/json',
   //   "Access-Control-Allow-Origin": "*",
