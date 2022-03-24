664a0051546c733b860c10beaf5c6d4135d72b31
Evgenij Kobzev <you@gmail.com>
2022-03-24 23:21:50 +0300

    new client v12.0.0(3)

--- src/components/Products/Products.js
+++ src/components/Products/Products.js
@@ -66,7 +66,7 @@ export default function Products() {
       <div className="container">
         <div className={s.products_row}>
 
-        <div style="width:100%;height:0;padding-bottom:100%;position:relative;"><iframe src="https://giphy.com/embed/jdPMeyv9rn0hZHh8n9" width="100%" height="100%" style="position:absolute" frameBorder="0" class="giphy-embed" allowFullScreen></iframe></div><p><a href="https://giphy.com/stickers/coding-programmer-ngoding-jdPMeyv9rn0hZHh8n9">via GIPHY</a></p>
+
           <div className={s.products_columns}>
             <div className={s.products_body}>
               {
