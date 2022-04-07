778ce63dc753dc9b833c46f0eca04dc9f3ae3f95
Evgenij Kobzev <you@gmail.com>
2022-04-07 21:51:43 +0300

    commit new-6

--- src/http/index.js
+++ src/http/index.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const host = axios.create({
   // withCredentials: true,
-  baseURL: 'https://student-store.ru'
+  baseURL: 'http://62.113.107.8/'
   // withCredentials: true,
   // withCredentials: false,
   // credentials: 'same-origin',
